55d16f075a7eb4255357ca0c2dee57f278073cc0
Blaine Mucklow <bmucklow@gmail.com>
2012-10-10 22:23:31 -0400

    Go on...

---
+++ Sensory/src/main/java/org/ffl/sensory/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.ffl.sensory;
+
+/**
+ * User: bmucklow
+ * Date: 10/10/12
+ * Time: 10:17 PM
+ */
+public class HelloWorld {
+    public void hello() {
+        System.out.println("Hello World");
+    }
+}
